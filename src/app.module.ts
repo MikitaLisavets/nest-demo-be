f10e21c57d2d8551fbd74771696df251a9b5ce81
Mikita Lisavets <mikita.lisavets@gmail.com>
2022-04-16 09:03:02 +0300

    init commit

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      //Replace this line with the one Cluster > Connect > Connect your Application
+      `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@cluster0.zlho5.mongodb.net/${process.env.MONGO_ATLAS_DB}?retryWrites=true&w=majority`,
+      ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
